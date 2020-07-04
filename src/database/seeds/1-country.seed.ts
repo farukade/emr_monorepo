9fbe2e7fb0ab764685290d770d945ab928116ac1
elsopi <elsopi02@gmail.com>
2020-07-04 18:40:53 +0100

    fix: fixed typeorm seeding issue

--- src/database/seeds/1-country.seed.ts
+++ src/database/seeds/1-country.seed.ts
@@ -2,23 +2,17 @@ import fs = require('fs');
 import { Connection } from 'typeorm';
 import { Factory, Seeder } from 'typeorm-seeding';
 import { Country } from '../../common/entities/country.entity';
-import {State} from "../../common/entities/state.entity";
 
 export default class CreateCountries implements Seeder {
   public async run(factory: Factory, connection: Connection): Promise<any> {
     const countries = JSON.parse(
-      fs.readFileSync('src/database/seeds/dumbs/countries.json', 'utf8'),
+        fs.readFileSync('src/database/seeds/dumbs/countries.json', 'utf8'),
     );
-    for (const i in countries) {
-      try {
-        const c = countries[i];
-        const country = new Country();
-        country.id = c.id;
-        country.name = c.name;
-        country.save();
-      } catch (error) {
-        continue;
-      }
-    }
+    await connection
+        .createQueryBuilder()
+        .insert()
+        .into(Country)
+        .values(countries)
+        .execute();
   }
 }
