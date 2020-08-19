08f0f63242366c228696e324904cea81b35dd52e
elsopi <elsopi02@gmail.com>
2020-08-19 10:42:28 +0100

    update migration script

--- src/database/migration/1597829843297-AppointmentTableUpdate.ts
+++ src/database/migration/1597829843297-AppointmentTableUpdate.ts
@@ -4,7 +4,6 @@ export class AppointmentTableUpdate1597829843297 implements MigrationInterface {
     name = 'AppointmentTableUpdate1597829843297'
 
     public async up(queryRunner: QueryRunner): Promise<any> {
-        await queryRunner.query(`ALTER TABLE "appointments" DROP COLUMN "doctorStatus"`, undefined);
         await queryRunner.query(`ALTER TABLE "appointments" ADD "doctorStatus" smallint NOT NULL DEFAULT 0`, undefined);
         await queryRunner.query(`ALTER TABLE "transactions" DROP COLUMN "next_location"`, undefined);
         await queryRunner.query(`ALTER TABLE "transactions" ADD "next_location" character varying`, undefined);
