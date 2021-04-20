20ca7d99ac4499e38952d13fca8e77ee07a038c4
Nsikan Ikpoh <nsikanikpoh@gmail.com>
2021-04-20 14:36:03 +0100

    added other emails

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -20,4 +20,25 @@ export class AppController {
         return { name: 'Patient Name', 
         address:'#34 Goas H as ahhsjsa ashashjas', date:'12-09-2021', total: 7829382.00, email:'nsdsnaikasoh@hajshjas.shajsh'};
     }
+
+    @Get('reg')
+    @Render('mail/reg')
+    reg() {
+        return { name: 'Reina Puiq', 
+        date:'12-09-2021', folderNumber:'67923782'};
+    }
+
+    @Get('pharmacy')
+    @Render('mail/pharmacy')
+    pharmacy() {
+        return { name: 'Reina Puiq', 
+        date:'12-09-2021', folderNumber:'67923782', total:89023.32, drugs:[{name:'Clamodia', amount:'3450.28', prescription:'2-2'}, {name:'Primora', amount:'2200.28', prescription:'1-1-1'}]};
+    }
+
+    @Get('discharge')
+    @Render('mail/discharge')
+    discharge() {
+        return { name: 'Reina Puiq', 
+        date:'12-09-2021', folderNumber:'67923782', total:89023.32, services:[{name:'Clamodia', amount:'3450.28'}, {name:'Consultation', amount:'2200.28'}]};
+    }
 }
--- src/modules/mail/mail.service.ts
+++ src/modules/mail/mail.service.ts
@@ -10,7 +10,7 @@ export class MailService {
 
         await this.mailerService.sendMail({
             to: data.email,
-            // from: '"Support Team" <support@example.com>', // override default from
+            from: '"DEDA Hospital" <info@dedahospital.com>', // override default from
             subject: 'Deda Hospital Invoice',
             template: 'invoice',
             context: {
@@ -20,6 +20,54 @@ export class MailService {
                 invoiceNumber: data.invoiceNumber,
                 email: data.email,
                 date: data.date,
+            },
+        });
+    }
+
+
+    async regMail(data: any) {
+
+        await this.mailerService.sendMail({
+            to: data.email,
+            from: '"DEDA Hospital" <info@dedahospital.com>', // override default from
+            subject: 'Welcome to Deda Hospital',
+            template: 'reg',
+            context: {
+                name: data.name,
+                folderNumber: data.folderNumber,
+                date: data.date,
+            },
+        });
+    }
+
+    async pharmacyMail(data: any) {
+
+        await this.mailerService.sendMail({
+            to: data.email,
+            from: '"DEDA Hospital" <info@dedahospital.com>', // override default from
+            subject: 'Drugs Purchase - Deda Pharmacy',
+            template: 'pharmacy',
+            context: {
+                name: data.name,
+                folderNumber: data.folderNumber,
+                date: data.date,
+                drugs: data.drugs,
+            },
+        });
+    }
+
+    async dischargeMail(data: any) {
+
+        await this.mailerService.sendMail({
+            to: data.email,
+            from: '"DEDA Hospital" <info@dedahospital.com>', // override default from
+            subject: 'Pay Later Bill - Deda Hospital',
+            template: 'discharge',
+            context: {
+                name: data.name,
+                folderNumber: data.folderNumber,
+                date: data.date,
+                services: data.services,
             },
         });
     }
