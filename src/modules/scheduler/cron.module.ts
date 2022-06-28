fc464208d25b823de70b291ae5ce66f39174dae5
omar-faruk-tech <farukadekunle@gmail.com>
2022-06-28 13:07:17 +0100

    hr staff attendance updated

--- src/modules/scheduler/cron.module.ts
+++ src/modules/scheduler/cron.module.ts
@@ -2,13 +2,15 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AttendanceRepository } from '../hr/attendance/repositories/attendance.repository';
 import { DeviceRepository } from '../hr/attendance/repositories/device.repositories';
+import { StaffRepository } from '../hr/staff/staff.repository';
 import { TasksService } from './cron.service';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([
     AttendanceRepository,
-    DeviceRepository
+    DeviceRepository,
+    StaffRepository
   ])],
   providers: [TasksService],
 })
